85f6f378fbc3aafa5606ed0ce6301f96ae4b2aa5
rawbeeni <elouazzanifarouk03@gmail.com>
2024-05-28 19:49:48 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Container } from 'reactstrap';
+import { getTokenOrRefresh } from './token_util';
+import './custom.css';
+import { ResultReason } from 'microsoft-cognitiveservices-speech-sdk';
+
+const speechsdk = require('microsoft-cognitiveservices-speech-sdk');
+
+export default function App() {
+    const [displayText, setDisplayText] = useState('INITIALIZED: ready to test speech...');
+    const playerRef = useRef({ p: undefined, muted: false });
+
+    useEffect(() => {
+        
+
+        
+        
+    }, []);
+
+    async function sttFromMic() {
+        const tokenObj = await getTokenOrRefresh();
+        const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
+        speechConfig.speechRecognitionLanguage = 'en-US';
+
+        const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
+        const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
+
+        setDisplayText('speak into your microphone...');
+
+        recognizer.recognizeOnceAsync(result => {
+            if (result.reason === ResultReason.RecognizedSpeech) {
+                setDisplayText(`RECOGNIZED: Text=${result.text}`);
+                window.botpressWebChat.sendPayload({
+                    type: 'trigger',
+                    payload: { sttquestion: result.text }
+                });
+            } else {
+                setDisplayText('ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.');
+            }
+        });
+    }
+
+    
+
+    
+
+    return (
+        <Container className="app-container">
+            <h1 className="display-4 mb-3">Speech sample app</h1>
+
+            <div className="row main-container">
+                <div className="col-6">
+                    <i className="fas fa-microphone fa-lg mr-2" onClick={() => sttFromMic()}></i>
+                    Convert speech to text from your mic.
+                    
+                </div>
+                <div className="col-6 output-display rounded">
+                    <code>{displayText}</code>
+                </div>
+            </div>
+        </Container>
+    );
+}
