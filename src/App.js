ff299d7ab634ef159807f689c72ec3cbf95aafa4
rawbeeni <elouazzanifarouk03@gmail.com>
2024-06-01 01:03:02 +0800

    TTS update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const speechsdk = require('microsoft-cognitiveservices-speech-sdk');
 
 export default function App() {
     const [displayText, setDisplayText] = useState('INITIALIZED: ready to test speech...');
-    
+    const [player, updatePlayer] = useState({p: undefined, muted: false});
 
     async function sttFromMic() {
         const tokenObj = await getTokenOrRefresh();
@@ -27,13 +27,46 @@ export default function App() {
                     type: 'trigger',
                     payload: { sttquestion: result.text }
                 });
+                window.botpressWebChat.onEvent(event => {
+                    if (event.type==='TRIGGER' && event.value.generated_response) {
+                        textToSpeech(event.value.generated_response);
+                }});
             } else {
                 setDisplayText('ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.');
             }
         });
     }
 
-    
+    async function textToSpeech(textToSpeak) {
+        const tokenObj = await getTokenOrRefresh();
+        const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
+        const myPlayer = new speechsdk.SpeakerAudioDestination();
+        updatePlayer(p => {p.p = myPlayer; return p;});
+        const audioConfig = speechsdk.AudioConfig.fromSpeakerOutput(player.p);
+
+        let synthesizer = new speechsdk.SpeechSynthesizer(speechConfig, audioConfig);
+
+        setDisplayText(`speaking text: ${textToSpeak}...`);
+        synthesizer.speakTextAsync(
+        textToSpeak,
+        result => {
+            let text;
+            if (result.reason === speechsdk.ResultReason.SynthesizingAudioCompleted) {
+                text = `synthesis finished for "${textToSpeak}".\n`
+            } else if (result.reason === speechsdk.ResultReason.Canceled) {
+                text = `synthesis failed. Error detail: ${result.errorDetails}.\n`
+            }
+            synthesizer.close();
+            synthesizer = undefined;
+            setDisplayText(text);
+        },
+        function (err) {
+            setDisplayText(`Error: ${err}.\n`);
+
+            synthesizer.close();
+            synthesizer = undefined;
+        });
+    }
 
     
 
