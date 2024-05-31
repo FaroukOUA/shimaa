c0cfbdf93c922e147c726aa85ec421b50a70743e
rawbeeni <152518773+rawbeeni@users.noreply.github.com>
2024-05-31 15:17:33 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import { Container } from 'reactstrap';
 import { getTokenOrRefresh } from './token_util';
 import './custom.css';
